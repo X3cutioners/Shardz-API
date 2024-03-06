55544f6ef8d457828f52f168b91cec779892d1f1
Abhishek Verma <w3abhishekmail@gmail.com>
2024-03-06 09:23:52 +0530

    Changed Shardz URL

--- send_email.py
+++ send_email.py
@@ -42,7 +42,7 @@ def send_verification_email(name, email, token):
 <p>Hi <strong>{name}</strong>,</p>
 <p data-sourcepos="7:1-7:132"><span class="pending">Thanks for signing up for Shardz!</span><span class="pending"> We're excited to help you unlock the power of unified storage across your favorite cloud services.</span></p>
 <p data-sourcepos="9:1-9:169"><span class="pending">Before you can start using the power of Shardz</span><span class="pending">,</span><span class="pending"> we need to confirm your email address.</span><span class="pending"> Just click the button below to verify your account:</span></p>
-<p data-sourcepos="9:1-9:169"><a href="https://shardz.io/verify?token={token}"><strong><span class="pending">Verify email</span></strong></a></p>
+<p data-sourcepos="9:1-9:169"><a href="https://shardz.moogi.tech/verify?token={token}"><strong><span class="pending">Verify email</span></strong></a></p>
 <p data-sourcepos="13:1-13:88"><span class="pending">Once you click the button,</span><span class="pending"> you'll be automatically redirected to Shardz and ready to go!</span></p>
 <p data-sourcepos="15:1-15:98"><strong class="pending">Please note:</strong><span class="pending"> If you don't verify your email, you'll not be able to use our services.</span></p>
 <p data-sourcepos="17:1-17:117"><span class="pending">If you're having trouble verifying your email,</span><span class="pending"> please reply to this message or contact us at <a href="https://telegram.me/w3Abhishek" target="_blank">Telegram</a>.</span></p>
@@ -65,7 +65,7 @@ def send_password_reset_email(name, email, token, ip2l):
         <p>Hi <strong>{name}</strong>,</p>
         <p data-sourcepos="7:1-7:132"><span class="pending">Have you forgot your password?</span><span class="pending"> No worries, you can always get a new one for your Shardz account.</span></p>
         <p data-sourcepos="9:1-9:169"><span class="pending">To reset your password, click on the following link:</span></p>
-        <p data-sourcepos="9:1-9:169"><a href="https://shardz.io/reset-password?token={token}"><strong><span class="pending">Reset password</span></strong></a></p>
+        <p data-sourcepos="9:1-9:169"><a href="https://shardz.moogi.tech/reset-password?token={token}"><strong><span class="pending">Reset password</span></strong></a></p>
         <p data-sourcepos="13:1-13:88"><span class="pending">{ip2l}</p>
         <p data-sourcepos="15:1-15:98"><strong class="pending">Please note:</strong><span class="pending"> If you didn't request a password reset, you can ignore this email.</span></p>
         <p data-sourcepos="17:1-17:117"><span class="pending">If you're having trouble resetting password,</span><span class="pending"> please reply to this message or contact us at <a href="https://telegram.me/w3Abhishek" target="_blank">Telegram</a>.</span></p>
